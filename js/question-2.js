0174745a17b2cef714260cd292f2faba3803fa0d
Karla <73695367+Noizenne@users.noreply.github.com>
2021-10-03 22:43:32 +0200

    Fixed
    Was missing += in the results div

--- js/question-2.js
+++ js/question-2.js
@@ -10,35 +10,27 @@ async function gamesList() {
 
     try {
         const response = await fetch(url);
-        
-        const results = await response.json();
 
-        console.log(results);
+        const results = await response.json();
 
         const games = results.results;
 
-        console.log(games.length);
+        const loading = document.querySelector(".loader");
+
+        loading.classList.remove("loader");
 
         for (let i = 0; i < games.length; i++) {
-
-            console.log(games[i].name);
-            console.log(games[i].rating);
-            console.log(games[i].tags.length);
 
             if ( i === 8) {
                 break;
             }
 
-            resultsContainer.innerHTML = `
+            resultsContainer.innerHTML += `
             <div class="results">
             <h2>Name: ${games[i].name}</h2>
             <h2>Rating: ${games[i].rating}</h2>
             <h2>Tags: ${games[i].tags.length}</h2>
             </div>`;
-
-            console.log(results[i]);
-
-
         }
     
     } catch (error) {
